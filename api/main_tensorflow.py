1db7c05e467553f676d644342265da34093928f3
Grian Gajila <griangajila2@gmail.com>
2025-04-13 02:35:40 +0800

    wip

--- api/main_tensorflow.py
+++ api/main_tensorflow.py
@@ -5,20 +5,23 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
+import requests
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
+# origins = [
+#     "http://localhost",
+#     "http://localhost:3000",
+# ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+endpoint = "http://localhost:3000/v1/models/rice_model:predict"
 
 MODEL = tf.keras.models.load_model("../models/rice.keras")
 
@@ -47,7 +50,14 @@ async def predict(file: UploadFile = File(...)):
 
     img_batch = np.expand_dims(image, 0)  
     
-    predictions = MODEL.predict(img_batch)
+    json_data ={
+        "instance": img_batch.tolist()
+    }
+
+    response = requests.post(endpoint, json=json_data)
+    predictions = np.array(response.json()["predictions"][0])
+
+
     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
     confidence = float(np.max(predictions[0])) * 100
 
