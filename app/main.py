a95efe37e4ee16fcf34d7bdfc4ea30048c5ec4eb
Grian Gajila <griangajila2@gmail.com>
2025-04-15 12:02:39 +0800

    wip

--- app/main.py
+++ app/main.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
-import uvicorn
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
 import json
 
+
 PORT = 8000
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 with open("disease.json") as result:
     DISEASES_DETAILS = json.load(result)
@@ -57,7 +70,3 @@ async def predict(file: UploadFile = File(...)):
         "confidence": f"{confidence:.2f}",
         "details": details
     }
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
